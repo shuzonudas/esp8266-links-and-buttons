0ebf364aafff75782668d50b3de17578e535a540
shuzonudas <shuzonudas@yahoo.com>
2016-05-25 23:36:40 +0600

    Add files via upload

---
+++ LCD.h
@@ -0,0 +1,48 @@
+// LCD.h
+
+#ifndef _LCD_h
+#define _LCD_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+
+void print(int x);
+void print(const char *characters);
+void println(const char *characters);
+void drawCircleHelper(int16_t x0, int16_t y0, int16_t r, uint8_t cornername);
+void setTextSize(uint8_t x);
+void setCursor(uint8_t x, uint8_t y);
+void fillRect(int16_t x, int16_t y, int16_t w, int16_t h);
+void fillRoundRect(int16_t x, int16_t y, int16_t w, int16_t h, int16_t r);
+void drawRoundRect(int16_t x, int16_t y, int16_t w, int16_t h, int16_t r);
+void drawRect(int16_t x, int16_t y, int16_t w, int16_t h);
+void drawFastHLine(int16_t x, int16_t y, int16_t w);
+void fillCircle(int16_t x0, int16_t y0, int16_t r);
+void fillCircleHelper(int16_t x0, int16_t y0, int16_t r, uint8_t cornername, int16_t delta);
+void drawFastVLine(int16_t x, int16_t y, int16_t h);
+void drawCircleHelper(int16_t x0, int16_t y0, int16_t r, uint8_t cornername);
+void drawCircle(int16_t x0, int16_t y0, int16_t r);
+void setBackColor(uint8_t red, uint8_t green, uint8_t blue);
+void fillScreen(void);
+void drawLine(int16_t x0, int16_t y0, int16_t x1, int16_t y1);
+void rotate(byte _rotation);
+void println(char *characters);
+void print(char *characters);
+void character(char character);
+void drawFontPixel(uint8_t x, uint8_t y, uint8_t red, uint8_t green, uint8_t blue, uint8_t pixelSize);
+void setColor(uint8_t red, uint8_t green, uint8_t blue);
+void setAddrWindow(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1);
+void drawPixel(int16_t x, int16_t y);
+void drawPixel(int16_t x, int16_t y, uint8_t red, uint8_t green, uint8_t blue);
+void init_LCD(void);
+void data(byte payload);
+void cmd(byte payload);
+void write(byte payload, byte dc);
+
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
---
+++ esp8266_buttons_links.ino
@@ -0,0 +1,120 @@
+#include "LCD.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid = "BTCL ADSL";
+const char* pass = "UHUBFJ2DNG";
+
+ESP8266WebServer server(80);
+
+void root()
+{
+	char temp[400];
+
+	snprintf(temp, 400,
+		"<html>\
+  <head>\
+  </head>\
+  <body>\
+    <h1>How to html page really good</h1>\
+	<a href =\"/number?value=0\">Bright</a>\
+	<a href =\"/number?value=512\">Medium</a>\
+	<a href =\"/number?value=1023\">Low</a>\
+	<a href =\"/string?input=button\"> <button> <h3> button <\h3> </button> </a>\
+	<a href =\"/string?input=give me food\"> <button> <h3> food <\h3> </button> </a>\
+	<a href =\"/string?input=let me sleep\"> <button> <h3> sleep <\h3> </button> </a>\
+  </body>\
+</html>");
+	server.send(200, "text/html", temp);
+
+
+	/*
+	server.send(200, "text/plain", "1->Open /number?value=90 to print number on LCD \r\n\
+2->Open /string?input=putYourString to print text on LCD \
+		");
+	*/
+}
+
+void handle_string() {
+	char inputString[100];
+	server.arg("input").toCharArray(inputString, 99, 0);
+	Serial.println(inputString);
+
+	// print on the LCD
+	setCursor(0, 50);
+	print(inputString);
+	print("          ");
+
+	server.send(200, "text/plain",inputString);
+}
+
+void handle_number()
+{
+	String sval = server.arg("value");
+	String xval = server.arg("muga");
+
+	Serial.println(sval.toInt());
+	Serial.println(xval.toFloat());
+	int ival = sval.toInt();
+	Serial.println(ival);
+// print on the LCD
+	setCursor(0, 50);
+	print(ival);
+	print("          ");
+
+	analogWrite(13, ival);
+
+	server.send(200, "text/plain", String(ival, DEC));
+}
+
+void setup(void) {
+	Serial.begin(115200);
+	Serial.println("");
+
+	init_LCD();
+	rotate(1);
+	setBackColor(0x00, 0x00, 0x00);
+	fillScreen();
+	
+	setTextSize(2);
+	setColor(0xFF, 0xFF, 0xFF);
+	setCursor(0, 2);
+	println("Connecting..");
+	WiFi.begin(ssid, pass);
+	// Wait for connection
+	setCursor(0, 20);
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.print(".");
+		print(".");
+	}
+
+
+	Serial.println("");
+	Serial.print("Connected to ");
+	Serial.println(ssid);
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP());
+
+	setCursor(0, 2);
+	print("Connected to:");
+	println(ssid);
+
+	IPAddress ipadd;
+	char ipAddConverted2String[26];
+	ipadd = WiFi.localIP();
+	sprintf(ipAddConverted2String, "%d.%d.%d.%d", ipadd[0], ipadd[1], ipadd[2], ipadd[3]);
+	println(ipAddConverted2String);
+
+
+	server.on("/", root);
+	server.on("/number", handle_number);
+	server.on("/string", handle_string);
+	server.begin();
+	Serial.println("HTTP server started");
+
+}
+
+void loop(void) {
+	server.handleClient();
+}
